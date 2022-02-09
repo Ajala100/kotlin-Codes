3afbec8a2684656a83b88bd7dc7bb09f2c85fc4b
ajala ahmad <amadajala98@gmail.com>
2022-02-09 21:56:37 +0100

    Factorial Code

--- src/exercises/Factorial.kt
+++ src/exercises/Factorial.kt
@@ -1,2 +1,14 @@
 package exercises
 
+//fun factorial(number: Int): Int{
+//    var factor = number
+//    var total = 0
+//    while(factor != 0){
+//        total = number *(number - 1)
+//        factor--
+//    }
+//    return total
+//}
+//fun main(arg: Array<String>){
+//   var value =  factorial(5)
+//}
