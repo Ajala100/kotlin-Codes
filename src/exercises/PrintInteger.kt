e1002b1bc3edc4149d01d42f92e23c860a797868
ajala ahmad <amadajala98@gmail.com>
2022-02-11 02:19:03 +0100

    Print Integer

---
+++ src/exercises/PrintInteger.kt
@@ -0,0 +1,10 @@
+package exercises
+
+import java.util.Scanner;
+
+    fun main(arg: Array<String>) {
+        var scanner = Scanner(System.`in`)
+        println("Enter Valid Integer: ")
+        var integer: Int = scanner.nextInt();
+        println("User Input is: $integer");
+    }
