72ab0081b3aefd9d13b4342a365e2aefad037a38
ajala ahmad <ahmadajala98@gmail.com>
2022-02-11 16:34:32 +0100

    Updated code

--- src/exercises/VowelOrConsonant.kt
+++ src/exercises/VowelOrConsonant.kt
@@ -7,6 +7,6 @@ fun main(arg: Array<String>){
     var vowels = charArrayOf('a', 'e', 'i', 'o', 'u');
     println("Enter Alphabet")
     var alphabet: Char = scanner.next().single()
-    if (vowels.contains(alphabet)) println("$alphabet is a vowel") else println("$alphabet is a consonant")
+    if (vowels.contains(alphabet)) println("$alphabet is a vowel!!") else println("$alphabet is a consonant!!")
 
 }
