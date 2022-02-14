474f690809a6d20fe31ab6520ade1c127e2f6771
ajala ahmad <ahmadajala98@gmail.com>
2022-02-14 14:54:08 +0100

    Alphabet check Program

---
+++ src/exercises/CheckwhetherACharacterIsAnAlphabetOrNot.kt
@@ -0,0 +1,9 @@
+package exercises
+
+class CheckWhetherACharacterIsAnAlphabetOrNot {
+    fun main(arg: Array<String>){
+        val letters = charArrayOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+        'v', 'w', 'x', 'y', 'z')
+
+    }
+}
